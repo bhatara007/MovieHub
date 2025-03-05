a5ddb2bd74df2ff466c181a00bf1c01a52710682
GingleBee <narattarwan.p@arise.tech>
2025-03-06 00:15:23 +0700

    log activity lifecycle, back stack

--- app/src/main/java/com/arise/training/moviehub/DetailActivity.kt
+++ app/src/main/java/com/arise/training/moviehub/DetailActivity.kt
@@ -1,72 +1,52 @@
 package com.arise.training.moviehub
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        lifecycleScope.launch {
-            delay(3000)
-            goToMainActivity()
-//            finish()
-        }
-        Log.d("testapp splash", "onCreate")
+        Log.d("testapp detail", "onCreate")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("testapp splash", "onStart")
+        Log.d("testapp detail", "onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("testapp splash", "onResume")
+        Log.d("testapp detail", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("testapp splash", "onPause")
+        Log.d("testapp detail", "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("testapp splash", "onStop")
+        Log.d("testapp detail", "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("testapp splash", "onDestroy")
+        Log.d("testapp detail", "onDestroy")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("testapp splash", "onRestart")
-    }
-
-    fun goToMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra(EXTRA_NAME, "Bee Splash")
-        startActivity(intent)
-    }
-
-
-    companion object {
-        const val EXTRA_NAME = "EXTRA_HI"
+        Log.d("testapp detail", "onRestart")
     }
 }
--- app/src/main/java/com/arise/training/moviehub/MainActivity.kt
+++ app/src/main/java/com/arise/training/moviehub/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.arise.training.moviehub
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arise.training.moviehub.SplashActivity.Companion.EXTRA_NAME
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,66 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(STATE_COUNT)
+        }
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val textView = findViewById<TextView>(R.id.mainTv)
+        textView.text = name
+        val button = findViewById<Button>(R.id.mainBtn)
+        button.setOnClickListener {
+            count += 1
+            goToDetailActivity()
+            Log.d("testapp main", "onClick $count")
+        }
+
+        Log.d("testapp main", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("testapp main", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("testapp main", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("testapp main", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("testapp main", "onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(STATE_COUNT, count)
+        Log.d("testapp main", "onSaveInstanceState")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("testapp main", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("testapp main", "onRestart")
+    }
+
+    fun goToDetailActivity() {
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val STATE_COUNT = "STATE_COUNT"
     }
 }
