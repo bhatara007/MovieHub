6b98a7c0f2aae69c780de39cf7ddb524e87d5df2
GingleBee <narattarwan.p@arise.tech>
2025-03-11 10:56:17 +0700

    add viewModel, use timber log

--- app/src/main/java/com/arise/training/moviehub/MainActivity.kt
+++ app/src/main/java/com/arise/training/moviehub/MainActivity.kt
@@ -2,80 +2,84 @@ package com.arise.training.moviehub
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.arise.training.moviehub.SplashActivity.Companion.EXTRA_NAME
+import com.arise.training.moviehub.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    var count = 0
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         if (savedInstanceState != null) {
-            count = savedInstanceState.getInt(STATE_COUNT)
+            viewModel.setCounter(value = savedInstanceState.getInt(STATE_COUNT))
         }
 
         val name = intent.getStringExtra(EXTRA_NAME)
-        val textView = findViewById<TextView>(R.id.mainTv)
-        textView.text = name
-        val button = findViewById<Button>(R.id.mainBtn)
-        button.setOnClickListener {
-            count += 1
-            goToDetailActivity()
-            Log.d("testapp main", "onClick $count")
+
+        viewModel.number.observe(this) {
+            binding.mainTv.text = "count $it"
         }
 
-        Log.d("testapp main", "onCreate")
+        binding.mainBtn.setOnClickListener {
+            viewModel.counter()
+//            goToDetailActivity()
+            Timber.d("onClick")
+        }
+
+        Timber.d("onCreate")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("testapp main", "onStart")
+        Timber.d("onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("testapp main", "onResume")
+        Timber.d("onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("testapp main", "onPause")
+        Timber.d("onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("testapp main", "onStop")
+        Timber.d("onStop")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt(STATE_COUNT, count)
-        Log.d("testapp main", "onSaveInstanceState")
+        outState.putInt(STATE_COUNT, viewModel.number.value ?: 0)
+        Timber.d("onSaveInstanceState")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("testapp main", "onDestroy")
+        Timber.d("onDestroy")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("testapp main", "onRestart")
+        Timber.d("onRestart")
     }
 
     fun goToDetailActivity() {
